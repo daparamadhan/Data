631442e9da8d274bfa0dd26bf68ca7ea76a70025
daparamadhan <daparamdnnn@gmail.com>
2026-01-01 17:42:39 +0700

    perubahan kecil

--- Datas/Forms/MainForm.Designer.cs
+++ Datas/Forms/MainForm.Designer.cs
@@ -28,19 +28,33 @@ namespace Datas
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(4, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(407, 279);
+            this.dataGridView1.TabIndex = 0;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(804, 461);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "MainForm";
-            this.Text = "MainForm";
+            this.Text = "Jabar Clean";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- Datas/Forms/MainForm.cs
+++ Datas/Forms/MainForm.cs
@@ -1,13 +1,46 @@
-using System;
+﻿using System;
 using System.Windows.Forms;
+using MongoDB.Driver;
+using System.Collections.Generic;
 
 namespace Datas
 {
     public partial class MainForm : Form
     {
+        private IMongoCollection<Sampah> collection;
+
         public MainForm()
         {
             InitializeComponent();
+            KoneksiMongo();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        // ===============================
+        // KONEKSI MONGODB
+        // ===============================
+        private void KoneksiMongo()
+        {
+            string connectionUri =
+                "mongodb+srv://daparamdnnn_db_user:rxgTiPOuzcB5lFRe@jabar.mgjaupi.mongodb.net/?appName=Jabar";
+
+            var client = new MongoClient(connectionUri);
+
+            var database = client.GetDatabase("datas");      // DATABASE BARU
+            collection = database.GetCollection<Sampah>("sampah"); // COLLECTION BARU
+        }
+
+        // ===============================
+        // LOAD DATA KE DATAGRIDVIEW
+        // ===============================
+        private void LoadData()
+        {
+            var data = collection.Find(_ => true).ToList();
+            dataGridView1.DataSource = data;
         }
     }
 }
